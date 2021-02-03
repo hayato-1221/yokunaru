77865c96eeb608a4aa2e45d2da40e878c1d5b867
hayato-1221 <hiperformance888@gmail.com>
2021-02-03 22:45:19 +0900

    createアクションの修正

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,4 +2,18 @@ class ClientsController < ApplicationController
   def index
     @clients = Client.all
   end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    Client.create!(name: client_params[:name], gender: client_params[:gender], age: client_params[:age], address: client_params[:address])
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :gender, :age, :address)
+  end
 end
