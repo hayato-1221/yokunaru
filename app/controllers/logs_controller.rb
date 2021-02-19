ba69dff027a19eeeabdc9cde773850d52c1ac988
hayato-1221 <hiperformance888@gmail.com>
2021-02-19 20:14:28 +0900

    logs_controller.rbの修正

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,32 +1,35 @@
 class LogsController < ApplicationController
+  before_action :set_post only: %i[index new edit update destroy]
   def index
-    @client = Client.find(params[:client_id])
+    @client = current_user.clients.find(params[:client_id])
     @logs = @client.logs.order(:id)
   end
 
   def new
-    @client = Client.find(params[:client_id])
+    @client = current_user.clients.find(params[:client_id])
     @log = Log.new
   end
 
   def create
-    client = Client.find(params[:client_id])
+    client = current_user.clients.find(params[:client_id])
     @log = client.logs.create!(log_params)
   end
 
   def edit
-    @client = Client.find(params[:client_id])
+    @client = current_user.clients.find(params[:client_id])
     @log = Log.find(params[:id])
   end
 
   def update
-    log = Log.find(params[:id])
+    @client = current_user.clients.find(params[:client_id])
+    log = @client.logs.find(params[:id])
     log.update!(log_params)
     redirect_to client_logs_path
   end
 
   def destroy
-    log = Log.find(params[:id])
+    @client = current_user.clients.find(params[:client_id])
+    log = @client.logs.find(params[:id])
     log.destroy!
     redirect_to client_logs_path
   end
@@ -35,5 +38,9 @@ class LogsController < ApplicationController
 
   def log_params
     params.require(:log).permit(:date, :fee).merge(client_id: params[:client_id])
+  end
+
+  def set_post
+    @client = current_user.clients.find(params[:client_id])
   end
 end
